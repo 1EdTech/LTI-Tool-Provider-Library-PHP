6c5a558b851cb4160df27b22b0c5d5036136ae2c
Jeremy Lindblom <jeremeamia@gmail.com>
2016-10-21 16:35:27 -0700

    Added support for using custom HTTP clients in a backward-compatible way.
    Also added a test suite that covers the HTTPMessage and client classes.

--- src/HTTPMessage.php
+++ src/HTTPMessage.php
@@ -2,8 +2,12 @@
 
 namespace IMSGlobal\LTI;
 
+use IMSGlobal\LTI\HTTP\Client as HTTPClient;
+use IMSGlobal\LTI\HTTP\CurlClient;
+use IMSGlobal\LTI\HTTP\StreamClient;
+
 /**
- * Class to represent an HTTP message
+ * Class to represent an HTTP message.
  *
  * @author  Stephen P Vickers <svickers@imsglobal.org>
  * @copyright  IMS Global Learning Consortium Inc
@@ -13,167 +17,114 @@ namespace IMSGlobal\LTI;
  */
 class HTTPMessage
 {
+    /**
+     * @var HTTPClient The client used to send the request.
+     */
+    private static $httpClient;
 
-/**
- * True if message was sent successfully.
- *
- * @var boolean $ok
- */
+    /**
+     * @var bool True if message was sent successfully.
+     */
     public $ok = false;
 
-/**
- * Request body.
- *
- * @var request $request
- */
+    /**
+     * @var string|null Request body.
+     */
     public $request = null;
 
-/**
- * Request headers.
- *
- * @var request_headers $requestHeaders
- */
-    public $requestHeaders = '';
+    /**
+     * @var array Request headers.
+     */
+    public $requestHeaders = [];
 
-/**
- * Response body.
- *
- * @var response $response
- */
+    /**
+     * @var string|null Response body.
+     */
     public $response = null;
 
-/**
- * Response headers.
- *
- * @var response_headers $responseHeaders
- */
+    /**
+     * @var string Response headers.
+     */
     public $responseHeaders = '';
 
-/**
- * Status of response (0 if undetermined).
- *
- * @var status $status
- */
+    /**
+     * @var int Status of response (0 if undetermined).
+     */
     public $status = 0;
 
-/**
- * Error message
- *
- * @var error $error
- */
+    /**
+     * @var string Error message
+     */
     public $error = '';
 
-/**
- * Request URL.
- *
- * @var url $url
- */
-    private $url = null;
+    /**
+     * @var string Request URL.
+     */
+    public $url = null;
 
-/**
- * Request method.
- *
- * @var method $method
- */
-    private $method = null;
+    /**
+     * @var string Request method.
+     */
+    public $method = null;
 
-/**
- * Class constructor.
- *
- * @param string $url     URL to send request to
- * @param string $method  Request method to use (optional, default is GET)
- * @param mixed  $params  Associative array of parameter values to be passed or message body (optional, default is none)
- * @param string $header  Values to include in the request header (optional, default is none)
- */
-    function __construct($url, $method = 'GET', $params = null, $header = null)
+    /**
+     * Allows you to set a custom HTTP client.
+     * 
+     * @param HTTPClient|null $httpClient The HTTP client to use for sending message.
+     */
+    public static function setHttpClient(HTTPClient $httpClient = null)
     {
-
-        $this->url = $url;
-        $this->method = strtoupper($method);
-        if (is_array($params)) {
-            $this->request = http_build_query($params);
-        } else {
-            $this->request = $params;
-        }
-        if (!empty($header)) {
-            $this->requestHeaders = explode("\n", $header);
-        }
-
+        self::$httpClient = $httpClient;
     }
 
-/**
- * Send the request to the target URL.
- *
- * @return boolean True if the request was successful
- */
+    /**
+     * Retrieves the HTTP client used for sending the message. Creates a default client if one is not set.
+     * 
+     * @return HTTPClient
+     */
+    public static function getHttpClient()
+    {
+        if (!self::$httpClient) {
+            // @codeCoverageIgnoreStart
+            if (function_exists('curl_init')) {
+                self::$httpClient =  new CurlClient();
+            } elseif (ini_get('allow_url_fopen')) {
+                self::$httpClient =  new StreamClient();
+            } else {
+                throw new \RuntimeException('Cannot create an HTTP client, because neither cURL or allow_url_fopen are enabled.');
+            }
+            // @codeCoverageIgnoreEnd
+        }
+        
+        return self::$httpClient;
+    }
+
+    /**
+     * Class constructor.
+     *
+     * @param string $url     URL to send request to
+     * @param string $method  Request method to use (optional, default is GET)
+     * @param array|string  $params  Associative array of parameter values to be passed or message body (optional, default is none)
+     * @param array|string $header  Values to include in the request header (optional, default is none)
+     */
+    function __construct($url, $method = 'GET', $params = null, $header = null)
+    {
+        $this->url = $url;
+        $this->method = strtoupper($method);
+        $this->request = is_array($params) ? http_build_query($params) : $params;
+        if ($header && !is_array($header)) {
+            $this->requestHeaders = explode("\n", $header);
+        }
+    }
+
+    /**
+     * Send the request to the target URL.
+     *
+     * @return boolean True if the request was successful
+     */
     public function send()
     {
-
-        $this->ok = false;
-// Try using curl if available
-        if (function_exists('curl_init')) {
-            $resp = '';
-            $ch = curl_init();
-            curl_setopt($ch, CURLOPT_URL, $this->url);
-            if (!empty($this->requestHeaders)) {
-                curl_setopt($ch, CURLOPT_HTTPHEADER, $this->requestHeaders);
-            } else {
-                curl_setopt($ch, CURLOPT_HEADER, 0);
-            }
-            if ($this->method === 'POST') {
-                curl_setopt($ch, CURLOPT_POST, true);
-                curl_setopt($ch, CURLOPT_POSTFIELDS, $this->request);
-            } else if ($this->method !== 'GET') {
-                curl_setopt($ch, CURLOPT_CUSTOMREQUEST, $this->method);
-                if (!is_null($this->request)) {
-                    curl_setopt($ch, CURLOPT_POSTFIELDS, $this->request);
-                }
-            }
-            curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-            curl_setopt($ch, CURLINFO_HEADER_OUT, true);
-            curl_setopt($ch, CURLOPT_HEADER, true);
-            //curl_setopt($ch, CURLOPT_SSLVERSION,3);
-            $chResp = curl_exec($ch);
-            $this->ok = $chResp !== false;
-            if ($this->ok) {
-                $chResp = str_replace("\r\n", "\n", $chResp);
-                $chRespSplit = explode("\n\n", $chResp, 2);
-                if ((count($chRespSplit) > 1) && (substr($chRespSplit[1], 0, 5) === 'HTTP/')) {
-                    $chRespSplit = explode("\n\n", $chRespSplit[1], 2);
-                }
-                $this->responseHeaders = $chRespSplit[0];
-                $resp = $chRespSplit[1];
-                $this->status = curl_getinfo($ch, CURLINFO_HTTP_CODE);
-                $this->ok = $this->status < 400;
-                if (!$this->ok) {
-                    $this->error = curl_error($ch);
-                }
-            }
-            $this->requestHeaders = str_replace("\r\n", "\n", curl_getinfo($ch, CURLINFO_HEADER_OUT));
-            curl_close($ch);
-            $this->response = $resp;
-        } else {
-// Try using fopen if curl was not available
-            $opts = array('method' => $this->method,
-                          'content' => $this->request
-                         );
-            if (!empty($this->requestHeaders)) {
-                $opts['header'] = $this->requestHeaders;
-            }
-            try {
-                $ctx = stream_context_create(array('http' => $opts));
-                $fp = @fopen($this->url, 'rb', false, $ctx);
-                if ($fp) {
-                    $resp = @stream_get_contents($fp);
-                    $this->ok = $resp !== false;
-                }
-            } catch (\Exception $e) {
-                $this->ok = false;
-            }
-        }
-
-        return $this->ok;
-
+        return self::getHttpClient()->send($this);
     }
 
 }
